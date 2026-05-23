7accfd4fddae61bbcd3060335ca032043242c540
Rashid-bd <76661048+Rashid-bd@users.noreply.github.com>
2026-05-23 23:24:46 +0600

    copy feature added

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // function to get liked number as integer
 function getLike() {
   const like = document.getElementById("like-count").innerText;
@@ -108,3 +109,77 @@ document.getElementById("rail-heart").addEventListener('click', function(event){
   const allLike = emergency + like;
   document.getElementById("like-count").innerText = allLike;
 })
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// function to get copy number as integer
+function getCopy(){
+  const copy = document.getElementById("copy-count").innerText;
+  return parseInt(copy);
+}
+
+// function to copy
+function copyNumber(id){
+  const getNumber = document.getElementById(id).innerText;
+  navigator.clipboard.writeText(getNumber);
+  alert("Number Copied");
+
+  const copy = getCopy();
+  const newCopy = copy + 1;
+  document.getElementById("copy-count").innerText = newCopy;
+}
+
+// copy emergency number
+document.getElementById("emergency-copy").addEventListener('click', function(event){
+  event.preventDefault();
+  copyNumber("emergency-number");
+})
+
+// copy police number
+document.getElementById("police-copy").addEventListener('click', function(event){
+  event.preventDefault();
+  copyNumber("police-number");
+})
+
+// copy fire service number
+document.getElementById("fire-copy").addEventListener('click', function(event){
+  event.preventDefault();
+  copyNumber("fire-number");
+})
+
+// copy ambulance number
+document.getElementById("ambulance-copy").addEventListener('click', function(event){
+  event.preventDefault();
+  copyNumber("ambulance-number");
+})
+
+// copy women & child number
+document.getElementById("child-copy").addEventListener('click', function(event){
+  event.preventDefault();
+  copyNumber("child-number");
+})
+
+// copy anti-corruption number
+document.getElementById("anti-copy").addEventListener('click', function(event){
+  event.preventDefault();
+  copyNumber("anti-number");
+})
+
+// copy electricity number
+document.getElementById("electricity-copy").addEventListener('click', function(event){
+  event.preventDefault();
+  copyNumber("electricity-number");
+})
+
+// copy brac number
+document.getElementById("brac-copy").addEventListener('click', function(event){
+  event.preventDefault();
+  copyNumber("brac-number");
+})
+
+// copy railway number
+document.getElementById("railway-copy").addEventListener('click', function(event){
+  event.preventDefault();
+  copyNumber("railway-number");
+})
+
